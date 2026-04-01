24f211b527b99005ac4b91c21e5104152937b4db
fluoronator <66132286+fluoronator@users.noreply.github.com>
2026-04-01 09:14:35 -0500

    Implement device orientation handling for map rotation

--- js/orientation.js
+++ js/orientation.js
@@ -1,1 +1,36 @@
 
+let mapWrapper = document.getElementById("mapWrapper");
+
+function rotateMap(deg) {
+    mapWrapper.style.transform = "rotate(" + (-deg) + "deg)";
+}
+
+function handleOrientation(event) {
+
+    let heading;
+
+    if (event.webkitCompassHeading !== undefined) {
+        heading = event.webkitCompassHeading;
+    }
+    else if (event.alpha !== null) {
+        heading = 360 - event.alpha;
+    }
+
+    if (heading !== undefined) {
+        rotateMap(heading);
+    }
+}
+
+// iPhone permission
+if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+    document.body.addEventListener("click", function() {
+        DeviceOrientationEvent.requestPermission()
+        .then(function(permissionState) {
+            if (permissionState === 'granted') {
+                window.addEventListener("deviceorientation", handleOrientation);
+            }
+        });
+    }, { once: true });
+} else {
+    window.addEventListener("deviceorientation", handleOrientation);
+}
