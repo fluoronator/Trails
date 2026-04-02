76af8e6aed783494fa4b505b32d5a98c9ed5d8b8
fluoronator <66132286+fluoronator@users.noreply.github.com>
2026-04-02 09:49:27 -0500

    Update orientation.js

--- js/orientation.js
+++ js/orientation.js
@@ -33,7 +33,8 @@ function applyRotation(heading) {
     mapWrapper.style.transform = `scale(2) rotate(${-heading}deg)`;
 
     // North arrow counter-rotates to always visually point true north.
-    northArrow.style.transform = `rotate(${heading}deg)`;
+    const normalizedHeading = ((heading % 360) + 360) % 360;
+    northArrow.style.transform = `rotate(${normalizedHeading}deg)`;
 
     // Expose the current visual rotation so gps.js can correct drag vectors.
     window.mapRotationDeg = ((heading % 360) + 360) % 360;
